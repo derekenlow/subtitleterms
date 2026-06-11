f4ad7453d6e5aa67a51e8870cea4d7cb6e35ca78
Derek Enlow <derekenlow@gmail.com>
2026-06-11 02:36:24 -0600

    Refactored custom hook to be fully hatch.

--- vendors.py
+++ vendors.py
@@ -1,13 +1,12 @@
 import subprocess
 import shutil
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
-import tomllib
 
 
 class CustomBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
         install_vendors(
-            self.metadata.core.dependencies, get_ignore(self.metadata.config)
+            self.metadata.core.dependencies, self.config["ignore-dependencies"]
         )
 
 
@@ -18,19 +17,3 @@ def install_vendors(dependencies, ignore):
         print("uv is not found in PATH.")
     uv_args = ["uv", "pip", "install", "-t", "src/subtitleterms/vendor", *vendor]
     subprocess.run(uv_args)
-
-
-def get_ignore(config):
-    try:
-        ignore = config["tool"]["vendors"]["ignore"]
-    except KeyError:
-        ignore = []
-    return ignore
-
-
-if __name__ == "__main__":
-    with open("pyproject.toml", "rb") as f:
-        config = tomllib.load(f)
-    dependencies = config["project"]["dependencies"]
-    ignore = get_ignore(config)
-    install_vendors(dependencies, ignore)
