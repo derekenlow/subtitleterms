97932e2bbff73a9f00bd39a6b92e1094c30151d6
Derek Enlow <derekenlow@gmail.com>
2026-06-11 03:08:53 -0600

    Vendor path now configurable.

--- vendors.py
+++ vendors.py
@@ -4,16 +4,25 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 
 class CustomBuildHook(BuildHookInterface):
+    def clean(self, versions):
+        # TODO: Clean up vendors.
+        pass
+
     def initialize(self, version, build_data):
-        install_vendors(
-            self.metadata.core.dependencies, self.config["ignore-dependencies"]
-        )
+        self.install_vendors()
 
+    def finalize(self, version, build_data, artifact_path):
+        # TODO: Convert to zip, and (optionally?) extract to a given path,
+        # to be linked as an Anki plugin.
+        pass
 
-def install_vendors(dependencies, ignore):
-    vendor = [p for p in dependencies if all([ig not in p for ig in ignore])]
-    print(" ".join(vendor))
-    if not shutil.which("uv"):
-        print("uv is not found in PATH.")
-    uv_args = ["uv", "pip", "install", "-t", "src/subtitleterms/vendor", *vendor]
-    subprocess.run(uv_args)
+    def install_vendors(self):
+        dependencies = self.metadata.core.dependencies
+        ignore = self.config["ignore-dependencies"]
+        vendor_dir = self.config["vendor-directory"]
+        vendor = [p for p in dependencies if all([ig not in p for ig in ignore])]
+        print(" ".join(vendor))
+        if not shutil.which("uv"):
+            print("uv is not found in PATH.")
+        uv_args = ["uv", "pip", "install", "-t", vendor_dir, *vendor]
+        subprocess.run(uv_args)
